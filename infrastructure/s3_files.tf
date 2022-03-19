f098c44c1b2927d93eb6563cca140c8d880903a3
JoaoPauloSarzedasRibeiro <sarzedas.joaopaulo@gmail.com>
2022-03-19 18:09:59 -0300

    fix terraform state folder

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,12 +1,4 @@
 #Ingestão de um arquivo no bucket criado anteriormente utilizando codigo
-resource "aws_s3_bucket_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark_from_tf.py"
-  acl    = "private"
-  source = "../job_spark_enem2020.py"
-  etag   = filemd5("../job_spark_enem2020.py")
-}
-
 resource "aws_s3_bucket_object" "delta_insert" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/01_delta_spark_insert.py"
